edef4c0a0c6b679f4111ce92f2708507e05158df
zombodotcom <tommysciano@gmail.com>
2018-05-04 13:32:56 -0600

    Add files via upload

--- app/classes/controller/federation.php
+++ app/classes/controller/federation.php
@@ -15,6 +15,49 @@ class Controller_Federation extends Controller
 
 		return $layout;
 	}
+	public function action_derp()
+	{
+				$layout = View::forge('federation/derp');
+				// $attraction = Ormfedmodel::find($id, array('select' => array('attID', 'attName', 'dsc', 'state')));
+        // $temp = array();
+        // $temp['id'] = $attraction['attID'];
+        // $temp['name'] = $attraction['attName'];
+        // $temp['desc'] = $attraction['dsc'];
+        // $temp['state'] = $attraction['state'];
+        // return json_encode($temp);
+
+		return $layout;
+	}
+
+
+
+
+
+		public function action_addItem($attID, $username){
+
+				$Federation = new federation();
+					 $attName = Federation::getAttractionName($attID);
+				Federation::addItem($attID, $username, ($attName[0]['attName']));
+			   	Response::redirect('federation/cart/'.$username);
+			}
+
+			public function action_deleteComment($attID, $commentID){
+					$federation = new federation();
+					Federation::deleteComment($commentID);
+				   	Response::redirect('federation/attraction/'.$attID);
+				}
+
+
+				public function action_updateComment($attID, $commentID){
+			$Federation = new federation();
+			$updatedComment = Input::post("updateComment");
+			if(isset($updatedComment)){
+				Federation::updateComment($commentID, $updatedComment);
+			}
+		   	Response::redirect('federation/attraction/'.$attID);
+		}
+
+
 
 	public function action_map()
 	{
@@ -34,6 +77,14 @@ class Controller_Federation extends Controller
 
 		return $layout;
 	}
+	public function action_derpAttractions()
+	{
+        $layout = View::forge('federation/derpAttractions');
+
+		return $layout;
+	}
+
+
 	public function action_fullmap()
 	{
         $layout = View::forge('federation/fullmap');
@@ -78,4 +129,67 @@ public function action_attrimage($id)
 
         return $response;
 	}
+
+	public function action_loggedin()
+	{
+		$layout = View::forge('federation/derp');
+
+		$content = View::forge('federation/derp');
+		$layout->content = Response::forge($content);
+
+
+		return $layout;
+	}
+
+
+	public function action_logout()
+	{
+		$session = Session::instance();
+			$session->destroy();
+			$layout = View::forge('federation/derp');
+			$content = View::forge('federation/derp');
+			$federation = new federation();
+
+			$attractions = federation::getAttraction();
+			$layout->set_safe("attractions", $attractions);
+
+
+			$layout->content = Response::forge($content);
+			return $layout;
+
+	}
+
+
+
+	public function action_login(){
+
+			$username = Input::post('username');
+
+			$password = Input::post('password');
+
+			$Federation = new federation();
+
+			$users = federation::getUsers();
+			$status = false;
+			foreach( $users as $user){
+				if($user['password'] === md5($password)){
+					Session::set('username', $user['username']);
+					$status = true;
+
+					if($user['pwrLvl'] == 'admin'){
+						Session::set('admin', 1);
+					}
+				}
+			}
+
+				if($status)	{
+					Response::redirect("federation/derp.php");
+				}
+				else{
+				Response::redirect("https://html5zombo.com/");
+				}
+		}
+
+
+
 }
